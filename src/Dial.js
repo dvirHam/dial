66e583bb46c943a3a22537c1580825e4187f9869
Neil Atkinson <neil@bluespot.io>
2017-10-03 09:50:09 +0100

    Add Dial component

---
+++ src/Dial.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Dial extends Component {
+  render () {
+    return (
+      <div className="Dial">hello</div>
+    )
+  }
+}
+
+export default Dial
